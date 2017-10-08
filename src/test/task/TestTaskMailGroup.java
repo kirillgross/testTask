fa1152db4f95f8020544baf9d95009514844b22c
kirillgross <kirillgross@yandex.ru>
2017-10-09 02:50:16 +0300

    Add files via upload

--- src/test/task/TestTaskMailGroup.java
+++ src/test/task/TestTaskMailGroup.java
@@ -1,47 +1,45 @@
 package test.task;
 
 public class TestTaskMailGroup {
-	static String[] musicStyle = { "RnB", "Electrohuse", "Поп-музыка" };
+	final static String[] musicStyle = { "RnB", "Electrohuse", "Поп-музыка" };
 	static int[] dancers;
 	static int countDancers;
 	static int[] trekList = new int[20];
 
-	public static void main(String[] args) throws InterruptedException {
-		countDancers = genericCountDancers();
-		System.out.println(countDancers);
-		dancers = new int[countDancers];
-		dancers=genericDancers(countDancers);
-		trekList=genericTrekList();
+	public static void clubDance(int[] dancers, int[] trekList, String[] musicStyle) throws InterruptedException {
 		for (int numberOfTrek = 0; numberOfTrek < 20; numberOfTrek++) {
-			
-			System.out.println("Играет трек №"+ (numberOfTrek+1) + " в стиле " + musicStyle[trekList[numberOfTrek]]+":");
+			System.out.println(
+					"Играет трек №" + (numberOfTrek + 1) + ", стиль - " + musicStyle[trekList[numberOfTrek]] + ":");
 			System.out.println();
 			for (int numberDancer = 0; numberDancer < countDancers; numberDancer++) {
 				if (dancers[numberDancer] == trekList[numberOfTrek]) {
-					System.out.println("Танцор " + numberDancer + " танцует под " + musicStyle[trekList[numberOfTrek]]);
+					System.out
+							.println("Персонаж " + numberDancer + " танцует под " + musicStyle[trekList[numberOfTrek]]);
 				} else {
-					System.out.println("Танцор " + numberDancer + " пьёт водку в баре");
+					System.out.println("Персонаж " + numberDancer + " пьёт водку в баре");
 				}
 			}
 			Thread.sleep(5000);
 			System.out.println();
 
 		}
-		
-
 	}
-	
+
 	public static int[] genericTrekList() {
-		int [] trekList = new int[20];
-		for(int i = 0; i < 20; i++) {
+		int[] trekList = new int[20];
+		// Генерурем треклист, состояший из 20 треков, одного из трёх стилей
+		for (int i = 0; i < 20; i++) {
 			trekList[i] = (int) (Math.random() * 3);
+			// Для каждого трека рандомим номер стиля музыки
 		}
 		return trekList;
 	}
 
 	public static int genericCountDancers() {
-		int a = 5;
-		int b = 10;
+		int a = 0;
+		int b = 1000;
+		// Генерируем общее количество персонажей больше или равно 0 и строго меньше
+		// 1000
 		int countDancers = a + (int) (Math.random() * b);
 		return countDancers;
 	}
@@ -51,8 +49,17 @@ public class TestTaskMailGroup {
 		dancers = new int[countDancers];
 		for (int i = 0; i < countDancers; i++) {
 			dancers[i] = (int) (Math.random() * 3);
+			// Для каждого персонажа рандомим номер стиля музыки, под который он танцует
 		}
 		return dancers;
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		countDancers = genericCountDancers();// Генерируем количество персонажей
+		dancers = new int[countDancers];
+		dancers = genericDancers(countDancers);// Генерируем для каждого персонажа один из трёх стилей танцев
+		trekList = genericTrekList();// Генерируем треклист
+		clubDance(dancers,trekList,musicStyle);// Запускаем танцы
 	}
 
 }
